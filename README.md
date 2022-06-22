7c58e3ed18afcfc876928318da3f8e879c4a6e4c
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-06-22 18:56:52 -0400

    set up REST API endpoints using Express routing methods

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ install Express and set up a basic server
 # part 2: 
 use Express to serve static files from the public folder, set up Morgan logging middleware
 
+# part 3:
+set up REST API endpoints using Express routing methods
+
+# part 4:
+create an Express router module
+
 # notes
 express is not a core module but since it's been installed to node_modules, we don't need to give a file path when using:
 const express = require('express');
@@ -20,3 +26,7 @@ middleware function in express has access to 3 parameters: req(request object), 
 morgan('dev') configures morgan to log using the development version which will print some additional information to the screen
 
 __dirname variable is a special variable in node, whenever used, it will refer to the absolute path of the current directory of the file that it is in
+
+app.use(express.json()); -- when the server receives requests with json formatted data in the body, this middleware function will handle parsing that json data into javascript properties of the request object so that we can use that data in javascript
+
+after the express server handles the code inside the callback for the app.all method, once it hits the next function it will go to the next relavent routing method. That means if the request that came in was a POST request, then it would go from the app.all method to the app.post method skipping the app.get method since its not a GET request.
--- server.js
+++ server.js
@@ -9,9 +9,51 @@ const port = 3000;
 
 //express() function will return an express server application that is available to us under the variable name app
 const app = express();
-
 //insert morgan middleware instead of using callback middleware function
 app.use(morgan('dev'));
+app.use(express.json());
+
+//support for rest api endpoints; any http requests to this path will trigger this method
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next(); //pass control of the application routing to the next relevant routing method after this one, otherwise it will just stop here
+});
+//set up endpoint for GET request
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+//set up endpoint for POST request
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+//set up endpoint for PUT request
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403; //operation is not supported
+    res.end('PUT operation is not supported on /campsites');
+});
+//set up endpoint for DELETE request
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+//route parameter -- allow us to store whatever the client sends as the part of the path after the slash as a route parameter named campsiteId
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`); 
+});
+app.put('/campsites/:campsiteId', (req, res) => {
+    //write to the body
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} 
+        with description: ${req.body.description}`);
+});
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 //set up express to serve files from the public folder with the help of express.static can accomplish in a single line
 //single line is all we need to have express serve static files from the public folder
